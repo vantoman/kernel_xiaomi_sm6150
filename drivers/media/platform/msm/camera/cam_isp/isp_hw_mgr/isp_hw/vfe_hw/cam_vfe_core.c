4e4615298f46b6458cc46612a5213f14ce9630df
dianlujitao <dianlujitao@lineageos.org>
2018-03-06 19:41:20 +0800

    msm: camera: isp: Use boot clock for recording start time
    Our camera HAL uses boot time for buffer timestamp, rather than
    system monotonic time. This leads to issues as framework uses system
    monotonic time as reference start time for timestamp adjustment.
    This issue manifests itself when recording videos. The recorded
    videos will appear longer by the amount of time the device has
    been sleeping.
    
    Change-Id: Ia4fac1d48e2206a7befd0030585776371dd8c3a9

--- drivers/media/platform/msm/camera/cam_isp/isp_hw_mgr/isp_hw/vfe_hw/cam_vfe_core.c
+++ drivers/media/platform/msm/camera/cam_isp/isp_hw_mgr/isp_hw/vfe_hw/cam_vfe_core.c
@@ -441,7 +441,7 @@ void cam_isp_hw_get_timestamp(struct cam_isp_timestamp *time_stamp)
 {
 	struct timespec ts;
 
-	get_monotonic_boottime(&ts);
+	ktime_get_ts(&ts);
 	time_stamp->mono_time.tv_sec    = ts.tv_sec;
 	time_stamp->mono_time.tv_usec   = ts.tv_nsec/1000;
 }
