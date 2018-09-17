8b7c0f389a9550625481bb12430c5ee649d5432b
kdrag0n <dragon@khronodragon.com>
2018-09-16 19:06:35 -0700

    block: anxiety-iosched: make max_writes_starved sysfs tunable
    Allow userspace to tune max_writes_starved like other I/O schedulers.
    
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- block/anxiety-iosched.c
+++ block/anxiety-iosched.c
@@ -9,11 +9,15 @@
 #include <linux/slab.h>
 #include <linux/init.h>
 
-#define MAX_WRITES_STARVED 8
+/* default tunable values */
+static const unsigned int max_writes_starved = 8; /* max amount of times reads can starve pending writes */
 
 struct anxiety_data {
 	struct list_head queue[2];
 	unsigned int writes_starved;
+
+	/* tunables */
+	unsigned int max_writes_starved;
 };
 
 static void anxiety_merged_requests(struct request_queue *q, struct request *rq, struct request *next)
@@ -24,7 +28,7 @@ static void anxiety_merged_requests(struct request_queue *q, struct request *rq,
 static __always_inline struct request *anxiety_choose_request(struct anxiety_data *mdata)
 {
 	/* prioritize reads unless writes are exceedingly starved */
-	bool starved = mdata->writes_starved > MAX_WRITES_STARVED;
+	bool starved = mdata->writes_starved > mdata->max_writes_starved;
 
 	/* read */
 	if (!starved && !list_empty(&mdata->queue[READ])) {
@@ -91,6 +95,7 @@ static int anxiety_init_queue(struct request_queue *q, struct elevator_type *elv
 	if (!eq)
 		return -ENOMEM;
 
+	/* allocate data */
 	data = kmalloc_node(sizeof(*data), GFP_KERNEL, q->node);
 	if (!data) {
 		kobject_put(&eq->kobj);
@@ -98,16 +103,44 @@ static int anxiety_init_queue(struct request_queue *q, struct elevator_type *elv
 	}
 	eq->elevator_data = data;
 
+	/* initialize data */
 	INIT_LIST_HEAD(&data->queue[READ]);
 	INIT_LIST_HEAD(&data->queue[WRITE]);
 	data->writes_starved = 0;
+	data->max_writes_starved = max_writes_starved;
 
+	/* set the elevator to us */
 	spin_lock_irq(q->queue_lock);
 	q->elevator = eq;
 	spin_unlock_irq(q->queue_lock);
 
 	return 0;
 }
+
+/* sysfs tunables */
+static ssize_t anxiety_max_writes_starved_show(struct elevator_queue *e, char *page)
+{
+	struct anxiety_data *ad = e->elevator_data;
+
+	return snprintf(page, PAGE_SIZE, "%d\n", ad->max_writes_starved);
+}
+
+static ssize_t anxiety_max_writes_starved_store(struct elevator_queue *e, const char *page, size_t count)
+{
+	struct anxiety_data *ad = e->elevator_data;
+	int ret;
+
+	ret = kstrtouint(page, 0, &ad->max_writes_starved);
+	if (ret < 0)
+		return ret;
+
+	return count;
+}
+
+static struct elv_fs_entry anxiety_attrs[] = {
+	__ATTR(max_writes_starved, 0644, anxiety_max_writes_starved_show, anxiety_max_writes_starved_store),
+	__ATTR_NULL
+};
 
 static struct elevator_type elevator_anxiety = {
 	.ops = {
@@ -119,6 +152,7 @@ static struct elevator_type elevator_anxiety = {
 		.elevator_init_fn				= anxiety_init_queue,
 	},
 	.elevator_name = "anxiety",
+	.elevator_attrs = anxiety_attrs,
 	.elevator_owner = THIS_MODULE,
 };
 
