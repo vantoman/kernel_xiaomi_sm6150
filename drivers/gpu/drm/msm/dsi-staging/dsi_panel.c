2b649e0ed6b08c72dff80b667d0f23654bfc6d32
Adrian Salido <salidoa@google.com>
2019-03-04 11:22:34 -0800

    drm/msm/dsi-staging: use dedicated kthread for mode switch
    The dedicated thread allows better control of when switch happens with
    regard to the panel TE signal.
    
    Bug: 125014929
    Change-Id: Id9d5c45b5a52852b4538e093d52ccfa902b069a9
    Signed-off-by: Adrian Salido <salidoa@google.com>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -3947,6 +3947,8 @@ int dsi_panel_drv_deinit(struct dsi_panel *panel)
 		return -EINVAL;
 	}
 
+	dsi_panel_switch_destroy(panel);
+
 	mutex_lock(&panel->panel_lock);
 
 	rc = dsi_panel_bl_unregister(panel);
@@ -4714,6 +4716,11 @@ int dsi_panel_switch(struct dsi_panel *panel)
 		return -EINVAL;
 	}
 
+	if (panel->funcs && panel->funcs->mode_switch) {
+		rc = panel->funcs->mode_switch(panel);
+		return rc;
+	}
+
 	mutex_lock(&panel->panel_lock);
 
 	rc = dsi_panel_tx_cmd_set(panel, DSI_CMD_SET_TIMING_SWITCH);
@@ -4833,6 +4840,10 @@ int dsi_panel_pre_disable(struct dsi_panel *panel)
 
 error:
 	mutex_unlock(&panel->panel_lock);
+
+	if (!rc && panel->funcs && panel->funcs->pre_disable)
+		rc = panel->funcs->pre_disable(panel);
+
 	return rc;
 }
 
