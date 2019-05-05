e316ad350afcfd93ef415ec2d367f46c1d5a7424
Sultan Alsawaf <sultan@kerneltoast.com>
2019-05-04 21:29:14 -0700

    msm: kgsl: Relax CPU latency requirements to save power
    Relaxing the CPU latency requirement by about 500 us won't significantly
    hurt graphics performance. On the flip side, most SoCs have many idle
    levels just below 1000 us in latency, with deeper idle levels having
    latencies in excess of 2000 us. Changing the latency requirement to
    1000 us allows most SoCs to use their deepest sub-1000-us idle state
    while the GPU is active.
    
    Additionally, since the lpm driver has been updated to allow power
    levels with latencies equal to target latencies, change the wakeup
    latency from 101 to 100 for clarity.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/gpu/msm/adreno.c
+++ drivers/gpu/msm/adreno.c
@@ -1008,6 +1008,11 @@ static int adreno_of_get_power(struct adreno_device *adreno_dev,
 		&device->pwrctrl.pm_qos_active_latency))
 		device->pwrctrl.pm_qos_active_latency = 1000;
 
+	/* get pm-qos-cpu-mask-latency, set it to default if not found */
+	if (of_property_read_u32(node, "qcom,l2pc-cpu-mask-latency",
+		&device->pwrctrl.pm_qos_cpu_mask_latency))
+		device->pwrctrl.pm_qos_cpu_mask_latency = 1000;
+
 	/* get pm-qos-wakeup-latency, set it to default if not found */
 	if (of_property_read_u32(node, "qcom,pm-qos-wakeup-latency",
 		&device->pwrctrl.pm_qos_wakeup_latency))
