a5607def5df3c3b63162086957af65965d33d95a
Zachariah Kennedy <zkennedy87@gmail.com>
2019-05-09 20:24:29 +0000

    sched/fair.c: Don't allow SchedTune boosted tasks to be migrated to small cores
    We want boosted tasks to run on big cores. But CAF's load balancer
    changes do not account for SchedTune boosting, so this allows for
    boosted tasks to be migrated to a suboptimal core. Let's mitigate
    this by setting the LBF_IGNORE_BIG_TASKS for tasks migrating from a
    larger capacity core to a smaller one and to check if the task is
    SchedTune boosted. If both are true, do not migrate this task.
    
    Signed-off-by: Zachariah Kennedy <zkennedy87@gmail.com>
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -9169,6 +9169,11 @@ int can_migrate_task(struct task_struct *p, struct lb_env *env)
 		return 0;
 #endif
 
+	/* Dont allow boosted tasks to be pulled to small cores */
+	if (env->flags & LBF_IGNORE_BIG_TASKS &&
+		(schedtune_task_boost(p) > 0))
+		return 0;
+
 	if (task_running(env->src_rq, p)) {
 		schedstat_inc(p->se.statistics.nr_failed_migrations_running);
 		return 0;
