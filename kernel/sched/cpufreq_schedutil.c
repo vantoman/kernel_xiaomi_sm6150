7981b7efc3ab278bb0dcdc461adf583e948eab32
Yaroslav Furman <yaro330@gmail.com>
2019-06-02 23:10:51 +0300

    cpufreq: schedutil: remove tracing from sugov_update_commit
    With it Clang fails to inline this function.
    Checked using llvm-objdump.
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: AtomicXZ <achubadyal4@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -231,7 +231,6 @@ static void sugov_update_commit(struct sugov_policy *sg_policy, u64 time,
 				unsigned int next_freq)
 {
 	struct cpufreq_policy *policy = sg_policy->policy;
-	unsigned int cpu;
 
 	if (sg_policy->next_freq == next_freq)
 		return;
@@ -249,9 +248,6 @@ static void sugov_update_commit(struct sugov_policy *sg_policy, u64 time,
 			return;
 
 		policy->cur = next_freq;
-		for_each_cpu(cpu, policy->cpus) {
-			trace_cpu_frequency(next_freq, cpu);
-		}
 	} else {
 		if (use_pelt())
 			sg_policy->work_in_progress = true;
