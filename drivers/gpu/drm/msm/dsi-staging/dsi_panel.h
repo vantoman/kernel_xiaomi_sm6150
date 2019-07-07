190585166b0d35d9bbbcc62a240b9054a3a1eabc
Demon000 <demonsingur@gmail.com>
2019-07-08 01:54:18 +0200

    drm: msm: Handle ss dcs type
    Squashed with:
    Author: minz1 <emerytang@gmail.com>
    Date:   Sun Aug 18 21:32:11 2019 -0500
        drm: msm: Handle ss ea and eb dcs types
    
        Change-Id: Ia49000fededac8cc355ca0831b5506712c43205a
    
    Change-Id: I8a1748c7093fff79704a54119b90d9db9fa63235

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.h
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.h
@@ -121,6 +121,10 @@ struct dsi_backlight_config {
 	bool bl_inverted_dbv;
 
 	int en_gpio;
+
+	bool dcs_type_ss_ea;
+	bool dcs_type_ss_eb;
+
 	/* PWM params */
 	struct pwm_device *pwm_bl;
 	bool pwm_enabled;
