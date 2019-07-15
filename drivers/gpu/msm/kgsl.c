9288eae7ba2d0e03ffdac88226b70de5c3540560
Park Ju Hyung <qkrwngud825@gmail.com>
2019-07-16 03:07:37 +0900

    adreno: leave only /sys/kernel/debug/kgsl/proc/%d/mem for debugfs
    Others are unused
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -4761,9 +4761,6 @@ int kgsl_device_platform_probe(struct kgsl_device *device)
 	if (status)
 		return status;
 
-	/* Initialize logging first, so that failures below actually print. */
-	kgsl_device_debugfs_init(device);
-
 	status = kgsl_pwrctrl_init(device);
 	if (status)
 		goto error;
@@ -4908,7 +4905,6 @@ error_close_mmu:
 error_pwrctrl_close:
 	kgsl_pwrctrl_close(device);
 error:
-	kgsl_device_debugfs_close(device);
 	_unregister_device(device);
 	return status;
 }
@@ -4936,7 +4932,6 @@ void kgsl_device_platform_remove(struct kgsl_device *device)
 
 	kgsl_pwrctrl_close(device);
 
-	kgsl_device_debugfs_close(device);
 	_unregister_device(device);
 }
 EXPORT_SYMBOL(kgsl_device_platform_remove);
