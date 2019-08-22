d0373afa611a6e8788b4fe4234731221ba8aaa11
Tyler Nijmeh <tylernij@gmail.com>
2019-08-21 19:32:58 -0700

    oom_kill: Reap killed task memory by default
    Android already sets this parameter on boot, but just in case another
    ROM is used which doesn't set this parameter, the kernel will have set
    it by default.
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- mm/oom_kill.c
+++ mm/oom_kill.c
@@ -53,7 +53,7 @@
 int sysctl_panic_on_oom;
 int sysctl_oom_kill_allocating_task;
 int sysctl_oom_dump_tasks;
-int sysctl_reap_mem_on_sigkill;
+int sysctl_reap_mem_on_sigkill = 1;
 
 DEFINE_MUTEX(oom_lock);
 
