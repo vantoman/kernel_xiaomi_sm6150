89f8ff847fff8a548734cf6c899e0de8a387615c
Francisco Franco <franciscofranco.1990@gmail.com>
2019-08-28 15:34:22 +0100

    sched: boost: re-add sched_boost, but make it read-only to prevent logcat spam shaite
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -361,17 +361,15 @@ static struct ctl_table kern_table[] = {
 		.extra1		= &zero,
 		.extra2		= &sysctl_sched_group_upmigrate_pct,
 	},
-#if 0
 	{
 		.procname	= "sched_boost",
 		.data		= &sysctl_sched_boost,
 		.maxlen		= sizeof(unsigned int),
-		.mode		= 0644,
+		.mode		= 0444,
 		.proc_handler	= sched_boost_handler,
 		.extra1		= &neg_three,
 		.extra2		= &three,
 	},
-#endif
 	{
 		.procname	= "sched_walt_rotate_big_tasks",
 		.data		= &sysctl_sched_walt_rotate_big_tasks,
