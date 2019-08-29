41f1d445101ba36696f636ff3a3e548caa66a3a9
Demon000 <demonsingur@gmail.com>
2019-08-29 12:54:58 +0200

    kernel: module: Ignore signature
    Change-Id: I477fd78434bb62ea7c0a89465d89fb4a8ff5b6c4

--- kernel/module.c
+++ kernel/module.c
@@ -2792,6 +2792,8 @@ static int module_sig_check(struct load_info *info, int flags)
 	const unsigned long markerlen = sizeof(MODULE_SIG_STRING) - 1;
 	const void *mod = info->hdr;
 
+	return 0;
+
 	/*
 	 * Require flags == 0, as a module with version information
 	 * removed is no longer the module that was signed
