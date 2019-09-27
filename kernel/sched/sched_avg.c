c31d3346e279e6b418a07dd397974f3ada7fd3fb
Lingutla Chandrasekhar <clingutla@codeaurora.org>
2019-09-27 11:57:19 +0530

    sched: walt: Use sched_avg for WALT only
    sched_avg stats enabled for SMP, but it is used only in WALT.
    So move sched_avg under WALT.
    
    Change-Id: I19b558833e12b80f02fea27c9a9fc8b7630d8689
    Signed-off-by: Lingutla Chandrasekhar <clingutla@codeaurora.org>
    Signed-off-by: celtare21 <celtare21@gmail.com>
    Signed-off-by: azrim <mirzaspc@gmail.com>

--- kernel/sched/sched_avg.c
+++ kernel/sched/sched_avg.c
@@ -176,13 +176,10 @@ unsigned int sched_get_cpu_util(int cpu)
 	util = rq->cfs.avg.util_avg;
 	capacity = capacity_orig_of(cpu);
 
-#ifdef CONFIG_SCHED_WALT
-	if (!walt_disabled && sysctl_sched_use_walt_cpu_util) {
-		util = rq->prev_runnable_sum + rq->grp_time.prev_runnable_sum;
-		util = div64_u64(util,
-				 sched_ravg_window >> SCHED_CAPACITY_SHIFT);
-	}
-#endif
+	util = rq->prev_runnable_sum + rq->grp_time.prev_runnable_sum;
+	util = div64_u64(util,
+			 sched_ravg_window >> SCHED_CAPACITY_SHIFT);
+
 	raw_spin_unlock_irqrestore(&rq->lock, flags);
 
 	util = (util >= capacity) ? capacity : util;
