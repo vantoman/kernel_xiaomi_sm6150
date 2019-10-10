9c104f3a2ea98cc2b082758789db44e1b2d08d10
Vitaly Wool <vitalywool@gmail.com>
2019-10-10 23:11:43 +0300

    zsmalloc: add compaction and huge class callbacks
    Add compaction callbacks for zpool compaction API extension.
    Add huge_class_size callback too to be fully aligned.
    
    With these in place, we can proceed with ZRAM modification
    to use the universal (zpool) API.
    
    Signed-off-by: Vitaly Wool <vitalywool@gmail.com>
    Change-Id: I3747371f43199fc785d641d51fd99e93c56971a6

--- mm/zsmalloc.c
+++ mm/zsmalloc.c
@@ -461,6 +461,11 @@ static unsigned long zs_zpool_get_compacted(void *pool)
 	return stats.pages_compacted;
 }
 
+static size_t zs_zpool_huge_class_size(void *pool)
+{
+	return zs_huge_class_size(pool);
+}
+
 static struct zpool_driver zs_zpool_driver = {
 	.type =		"zsmalloc",
 	.owner =	THIS_MODULE,
@@ -476,6 +481,7 @@ static struct zpool_driver zs_zpool_driver = {
 	.compact =	zs_zpool_compact,
 	.get_num_compacted =	zs_zpool_get_compacted,
 	.total_size =		  zs_zpool_total_size,
+	.huge_class_size =	  zs_zpool_huge_class_size,
 };
 
 MODULE_ALIAS("zpool-zsmalloc");
