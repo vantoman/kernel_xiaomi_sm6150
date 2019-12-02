db07c7f1e0873e2b106444da56d1ccaa3efcb1aa
UtsavBalar1231 <utsavbalar1231@gmail.com>
2019-12-02 03:49:40 +0100

    input: fingerprint: goodix_ta: Switch to msm drm notifier api
    - use the qcom default notifier to remove dependency on xiaomi drm notifier
    
    Change-Id: I5dfef4717c9cd6d4d678f98c024e65faae21e83e
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/input/fingerprint/goodix_ta/gf_spi.c
+++ drivers/input/fingerprint/goodix_ta/gf_spi.c
@@ -47,7 +47,7 @@
 #include <linux/pm_wakeup.h>
 #include <drm/drm_bridge.h>
 #ifndef GOODIX_DRM_INTERFACE_WA
-#include <drm/drm_notifier.h>
+#include <linux/msm_drm_notify.h>
 #endif
 
 #include "gf_spi.h"
@@ -751,11 +751,11 @@ static int goodix_fb_state_chg_callback(struct notifier_block *nb,
 					unsigned long val, void *data)
 {
 	struct gf_dev *gf_dev;
-	struct fb_event *evdata = data;
+	struct msm_drm_notifier *evdata = data;
 	unsigned int blank;
 	char temp[4] = { 0x0 };
 
-	if (val != DRM_EVENT_BLANK) {
+	if (val != MSM_DRM_EVENT_BLANK) {
 		return 0;
 	}
 
@@ -763,11 +763,11 @@ static int goodix_fb_state_chg_callback(struct notifier_block *nb,
 		 __func__, (int)val);
 	gf_dev = container_of(nb, struct gf_dev, notifier);
 
-	if (evdata && evdata->data && val == DRM_EVENT_BLANK && gf_dev) {
+	if (evdata && evdata->data && val == MSM_DRM_EVENT_BLANK && gf_dev) {
 		blank = *(int *)(evdata->data);
 
 		switch (blank) {
-			case DRM_BLANK_POWERDOWN:
+			case MSM_DRM_BLANK_POWERDOWN:
 				if (gf_dev->device_available == 1) {
 					gf_dev->fb_black = 1;
 					gf_dev->wait_finger_down = true;
@@ -784,7 +784,7 @@ static int goodix_fb_state_chg_callback(struct notifier_block *nb,
 				}
 				break;
 
-			case DRM_BLANK_UNBLANK:
+			case MSM_DRM_BLANK_UNBLANK:
 				if (gf_dev->device_available == 1) {
 					gf_dev->fb_black = 0;
 #if defined(GF_NETLINK_ENABLE)
@@ -914,7 +914,7 @@ static int gf_probe(struct platform_device *pdev)
 #endif
 #ifndef GOODIX_DRM_INTERFACE_WA
 	gf_dev->notifier = goodix_noti_block;
-	drm_register_client(&gf_dev->notifier);
+	msm_drm_register_client(&gf_dev->notifier);
 #endif
 	gf_dev->irq = gf_irq_num(gf_dev);
 	wakeup_source_init(&fp_wakelock, "fp_wakelock");
@@ -979,7 +979,7 @@ static int gf_remove(struct platform_device *pdev)
 	}
 
 #ifndef GOODIX_DRM_INTERFACE_WA
-	drm_unregister_client(&gf_dev->notifier);
+	msm_drm_unregister_client(&gf_dev->notifier);
 #endif
 	mutex_unlock(&device_list_lock);
 	return 0;
