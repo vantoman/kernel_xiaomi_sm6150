8299815198df82c819b8b8461c15240277f741be
Yaroslav Furman <yaro330@gmail.com>
2019-12-12 23:44:10 +0300

    sched: provide a no-op sched_boost node
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: DarkDampSquib <andrin.geiger1998@gmail.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -308,6 +308,10 @@ static int max_sched_tunable_scaling = SCHED_TUNABLESCALING_END-1;
 #endif /* CONFIG_SMP */
 #endif /* CONFIG_SCHED_DEBUG */
 
+#ifndef CONFIG_SCHED_WALT
+static int sysctl_sched_boost = 0;
+#endif
+
 #ifdef CONFIG_COMPACTION
 static int min_extfrag_threshold;
 static int max_extfrag_threshold = 1000;
@@ -408,6 +412,16 @@ static struct ctl_table kern_table[] = {
 		.proc_handler	= sched_little_cluster_coloc_fmin_khz_handler,
 		.extra1		= &zero,
 		.extra2		= &two_million,
+	},
+#else
+	{
+		.procname	= "sched_boost",
+		.data		= &sysctl_sched_boost,
+		.maxlen		= sizeof(unsigned int),
+		.mode		= 0644,
+		.proc_handler	= sched_boost_handler,
+		.extra1		= &neg_three,
+		.extra2		= &three,
 	},
 #endif
 	{
@@ -3364,6 +3378,29 @@ int proc_douintvec_capacity(struct ctl_table *table, int write,
 {
 	return do_proc_dointvec(table, write, buffer, lenp, ppos,
 				do_proc_douintvec_capacity_conv, NULL);
+}
+
+int sched_boost_handler(struct ctl_table *table, int write,
+		void __user *buffer, size_t *lenp,
+		loff_t *ppos)
+{
+	int ret;
+	unsigned int *data = (unsigned int *)table->data;
+
+	ret = proc_dointvec_minmax(table, write, buffer, lenp, ppos);
+
+	if (ret || !write)
+		return ret;
+
+#if 0
+	if (verify_boost_params(*data))
+		_sched_set_boost(*data);
+	else
+		return -EINVAL;
+#endif
+
+	pr_info("%s set sb to %i\n", current->comm, *data);
+	return ret;
 }
 
 #else /* CONFIG_PROC_SYSCTL */
