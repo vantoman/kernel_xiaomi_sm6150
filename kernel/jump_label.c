b4115d66a117f2e2e19cb4c0cd3c7f23837b5f78
freak07 <michalechner92@googlemail.com>
2020-01-14 11:24:43 +0100

    kernel: use power efficient workingqueues
    (cherry picked from commit 28e2f86a8aff5ffb74b85f4e373609acd0c85931)
    (cherry picked from commit 5312bcd3348868f63d88b5d35ff737bda0f47947)
    Signed-off-by: CloudedQuartz <ravenklawasd@gmail.com>
    Signed-off-by: KenHV <yo@kenharris.xyz>

--- kernel/jump_label.c
+++ kernel/jump_label.c
@@ -199,7 +199,7 @@ static void __static_key_slow_dec_cpuslocked(struct static_key *key,
 
 	if (rate_limit) {
 		atomic_inc(&key->enabled);
-		schedule_delayed_work(work, rate_limit);
+		queue_delayed_work(system_power_efficient_wq, work, rate_limit);
 	} else {
 		jump_label_update(key);
 	}
