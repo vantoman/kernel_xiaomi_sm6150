6e83d2114e10ead031e8c1304f84c1ba411485d9
Puranam V G Tejaswi <pvgtejas@codeaurora.org>
2020-02-27 02:04:35 +0530

    msm: kgsl: do not set private flag for secure allocations
    The intention to set page private flag was to avoid
    importing secure buffers as non secure buffers. This would
    result in secure buffers being incorrectly mapped to a non
    secure context bank. This problem can arise when importing
    ion buffers, but never with a secure allocation from kgsl.
    There is no means to import a secure kgsl buffer as
    non secure buffer. So setting page private for secure
    allocations done by kgsl can be safely removed.
    
    Change-Id: I9ea879ec36f945a0f1fd57055dc7d4fa3bfe8905
    Signed-off-by: Puranam V G Tejaswi <pvgtejas@codeaurora.org>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- drivers/gpu/msm/kgsl_sharedmem.c
+++ drivers/gpu/msm/kgsl_sharedmem.c
@@ -487,12 +487,10 @@ done:
 
 int kgsl_lock_sgt(struct sg_table *sgt, uint64_t size)
 {
-	struct scatterlist *sg;
 	int dest_perms = PERM_READ | PERM_WRITE;
 	int source_vm = VMID_HLOS;
 	int dest_vm = VMID_CP_PIXEL;
 	int ret;
-	int i;
 
 	ret = hyp_assign_table(sgt, &source_vm, 1, &dest_vm, &dest_perms, 1);
 	if (ret) {
@@ -510,10 +508,6 @@ int kgsl_lock_sgt(struct sg_table *sgt, uint64_t size)
 		return ret;
 	}
 
-	/* Set private bit for each sg to indicate that its secured */
-	for_each_sg(sgt->sgl, sg, sgt->nents, i)
-		SetPagePrivate(sg_page(sg));
-
 	return 0;
 }
 
@@ -523,7 +517,6 @@ int kgsl_unlock_sgt(struct sg_table *sgt)
 	int source_vm = VMID_CP_PIXEL;
 	int dest_vm = VMID_HLOS;
 	int ret;
-	struct sg_page_iter sg_iter;
 
 	ret = hyp_assign_table(sgt, &source_vm, 1, &dest_vm, &dest_perms, 1);
 
@@ -532,8 +525,6 @@ int kgsl_unlock_sgt(struct sg_table *sgt)
 		return ret;
 	}
 
-	for_each_sg_page(sgt->sgl, &sg_iter, sgt->nents, 0)
-		ClearPagePrivate(sg_page_iter_page(&sg_iter));
 	return 0;
 }
 
@@ -1425,9 +1416,6 @@ static int kgsl_cma_alloc_secure(struct kgsl_device *device,
 	if (result != 0)
 		goto err;
 
-	/* Set the private bit to indicate that we've secured this */
-	SetPagePrivate(sg_page(memdesc->sgt->sgl));
-
 	memdesc->priv |= KGSL_MEMDESC_TZ_LOCKED;
 
 	/* Record statistics */
@@ -1452,8 +1440,7 @@ static void kgsl_cma_unlock_secure(struct kgsl_memdesc *memdesc)
 	if (memdesc->size == 0 || !(memdesc->priv & KGSL_MEMDESC_TZ_LOCKED))
 		return;
 
-	if (!scm_lock_chunk(memdesc, 0))
-		ClearPagePrivate(sg_page(memdesc->sgt->sgl));
+	scm_lock_chunk(memdesc, 0);
 }
 
 void kgsl_sharedmem_set_noretry(bool val)
