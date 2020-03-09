69b4910a21a4b0a1ad29f9f8c02a699d47c82656
Nauval Rizky <enuma.alrizky@gmail.com>
2020-03-09 11:54:52 +0900

    drm: msm: adapt exposure adjustment methods to courbet
    Signed-off-by: Nauval Rizky <enuma.alrizky@gmail.com>

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -49,6 +49,10 @@
 #include <linux/klapse.h>
 #endif
 
+#ifdef CONFIG_EXPOSURE_ADJUSTMENT
+#include "exposure_adjustment.h"
+#endif
+
 /**
  * topology is currently defined by a set of following 3 values:
  * 1. num of layer mixers
@@ -971,6 +975,7 @@ bool dc_set_backlight(struct dsi_panel *panel, u32 bl_lvl)
 int dsi_panel_set_backlight(struct dsi_panel *panel, u32 bl_lvl)
 {
 	int rc = 0;
+	int bl_dc_min = panel->bl_config.bl_min_level * 2;
 
 	struct dsi_backlight_config *bl = &panel->bl_config;
 
@@ -978,6 +983,11 @@ int dsi_panel_set_backlight(struct dsi_panel *panel, u32 bl_lvl)
 		return 0;
 
 	pr_debug("backlight type:%d lvl:%d\n", bl->type, bl_lvl);
+
+#ifdef CONFIG_EXPOSURE_ADJUSTMENT
+	if (bl_lvl > 0)
+		bl_lvl = ea_panel_calc_backlight(bl_lvl < bl_dc_min ? bl_dc_min : bl_lvl);
+#endif
 
 	if (dc_set_backlight(panel, bl_lvl)) {
 		panel->last_bl_lvl = bl_lvl;
