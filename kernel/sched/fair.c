78fcaa055986b26d5afada92c2254b1600b0421c
Wilson Sung <wilsonsung@google.com>
2020-03-09 17:12:05 +0800

    Roll back performace files
    File list:
    kernel/sched/
    include/linux/sched/
    
    Bug: 150756696
    Bug: 149660093
    
    Change-Id: Ia90f5123ef3ab0d3322e974d174b915e80ed642e
    Signed-off-by: Wilson Sung <wilsonsung@google.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -8213,7 +8213,6 @@ static int find_energy_efficient_cpu(struct sched_domain *sd,
 	int boosted = (schedtune_task_boost(p) > 0);
 
 	fbt_env.fastpath = 0;
-	fbt_env.need_idle = 0;
 
 	if (trace_sched_task_util_enabled())
 		start_t = sched_clock();
@@ -9326,7 +9325,6 @@ static void detach_task(struct task_struct *p, struct lb_env *env)
 	p->on_rq = TASK_ON_RQ_MIGRATING;
 	deactivate_task(env->src_rq, p, DEQUEUE_NOCLOCK);
 #ifdef CONFIG_SCHED_WALT
-	lockdep_off();
 	double_lock_balance(env->src_rq, env->dst_rq);
 	if (!(env->src_rq->clock_update_flags & RQCF_UPDATED))
 		update_rq_clock(env->src_rq);
