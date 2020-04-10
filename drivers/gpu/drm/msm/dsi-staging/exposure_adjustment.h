a6b27e8f71d3cb33f5ce10b38f519aafb1c44c32
Nauval Rizky <enuma.alrizky@gmail.com>
2020-04-11 00:37:41 +0900

    drm: exposure_adjustment: Reduce PCC coefficient floor
    Users seems care more about brightness levels rather than color
    reproduction, so let's turn it down so we can archive much lower
    backlight intensity on minimum brightness.
    
    Signed-off-by: Nauval Rizky <enuma.alrizky@gmail.com>

--- drivers/gpu/drm/msm/dsi-staging/exposure_adjustment.h
+++ drivers/gpu/drm/msm/dsi-staging/exposure_adjustment.h
@@ -27,7 +27,7 @@
  * @EXPOSURE_ADJUSTMENT_MIN: Minimum available PCC coefficient for OLED panel
  */
 #define ELVSS_OFF_THRESHOLD        610
-#define EXPOSURE_ADJUSTMENT_MIN    4096
+#define EXPOSURE_ADJUSTMENT_MIN    1024
 
 /* PCC coefficient when exposure is 255 */
 #define EXPOSURE_ADJUSTMENT_MAX    35700
