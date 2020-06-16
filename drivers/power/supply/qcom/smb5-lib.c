fd0023be8a9bc34d51d10a2c03249dbfd654ef43
Pranav Vashi <neobuddy89@gmail.com>
2020-06-16 21:19:56 +0530

    power/supply: Bound USB current only for USB charger type
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/power/supply/qcom/smb5-lib.c
+++ drivers/power/supply/qcom/smb5-lib.c
@@ -5414,10 +5414,11 @@ static int smblib_handle_usb_current(struct smb_charger *chg,
 				&& (usb_current == SUSPEND_CURRENT_UA))
 		is_float = true;
 
-	if (((usb_current < USBIN_500MA) && (usb_current > 0))
-			|| (usb_current == USBIN_900MA))
-	{
-		usb_current = USBIN_500MA;
+	if (chg->real_charger_type == POWER_SUPPLY_TYPE_USB) {
+		if (usb_current > 0 && usb_current < USBIN_500MA)
+			usb_current = USBIN_500MA;
+		else if (usb_current >= USBIN_500MA && usb_current < USBIN_900MA)
+			usb_current = USBIN_900MA;
 	}
 
 	if (chg->real_charger_type == POWER_SUPPLY_TYPE_USB_FLOAT) {
