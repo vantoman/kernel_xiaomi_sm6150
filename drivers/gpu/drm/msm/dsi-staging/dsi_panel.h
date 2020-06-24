bb1a8418ea28ab1b78d99618134743b43a620e4c
Arian <arian.kulmer@web.de>
2020-06-24 12:38:25 +0200

    drm: msm: Disable DC dimming while HBM is active
    Change-Id: I76f61f7de1637bd70c65a684468d41f24b03e15c

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.h
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.h
@@ -247,6 +247,7 @@ struct dsi_panel {
 	enum dsi_doze_mode_type doze_mode;
 	int hbm_mode;
 	bool resend_ea;
+	bool resend_ea_hbm;
 
 	struct brightness_alpha_pair *fod_dim_lut;
 	u32 fod_dim_lut_count;
