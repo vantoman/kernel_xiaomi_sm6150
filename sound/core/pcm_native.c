4c8ec6437a3b9ce6b076fb5115b9260f41999e05
celtare21 <celtare21@gmail.com>
2020-08-15 11:37:18 +0200

    sound: core: Affine pm_qos request to cpu0
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- sound/core/pcm_native.c
+++ sound/core/pcm_native.c
@@ -734,9 +734,15 @@ static int snd_pcm_hw_params(struct snd_pcm_substream *substream,
 
 	if (pm_qos_request_active(&substream->latency_pm_qos_req))
 		pm_qos_remove_request(&substream->latency_pm_qos_req);
-	if ((usecs = period_to_usecs(runtime)) >= 0)
+	if ((usecs = period_to_usecs(runtime)) >= 0) {
+		substream->latency_pm_qos_req.type = PM_QOS_REQ_AFFINE_CORES;
+
+		cpumask_empty(&substream->latency_pm_qos_req.cpus_affine);
+		cpumask_set_cpu(0, &substream->latency_pm_qos_req.cpus_affine);
+
 		pm_qos_add_request(&substream->latency_pm_qos_req,
 				   PM_QOS_CPU_DMA_LATENCY, usecs);
+	}
 	return 0;
  _error:
 	/* hardware might be unusable from this time,
