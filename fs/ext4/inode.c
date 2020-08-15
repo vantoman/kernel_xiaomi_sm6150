2836a2933bacadb335378ffc162f8838397df625
Adam W. Willis <return.of.octobot@gmail.com>
2020-08-15 13:44:58 +0200

    ext4: Drop unused variable
    The rw variable is no-op subsequent to 56eff50
    ("ext4: remove additional tracings added by CAF")
    
    This silences the following compiler warning:
    
    ../fs/ext4/inode.c:3805:6: warning: unused variable 'rw' [-Wunused-variable]
            int rw = iov_iter_rw(iter);
                ^
    
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>

--- fs/ext4/inode.c
+++ fs/ext4/inode.c
@@ -3826,7 +3826,6 @@ static ssize_t ext4_direct_IO(struct kiocb *iocb, struct iov_iter *iter)
 	size_t count = iov_iter_count(iter);
 	loff_t offset = iocb->ki_pos;
 	ssize_t ret;
-	int rw = iov_iter_rw(iter);
 
 #if defined(CONFIG_FS_ENCRYPTION)
 	if (IS_ENCRYPTED(inode) && S_ISREG(inode->i_mode)
