6043b414614597e370ab83162431a131ddfe7ad3
Signed-off-by: celtare21 <celtare21@gmail.com>
2020-08-16 17:26:15 +0200

    sched: tune: Drop top-app boost
     * We want ze battery.
    
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -740,7 +740,7 @@ static void write_default_values(struct cgroup_subsys_state *css)
 		{ "background",	0, 0 },
 		{ "foreground",	1, 0 },
 		{ "rt",		0, 0 },
-		{ "top-app",	5, 1 },
+		{ "top-app",	0, 1 },
 	};
 	int i;
 
