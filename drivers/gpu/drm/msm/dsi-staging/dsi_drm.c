d38323fed0217af55503d4635fa4820d6b6bdd8a
Arian <arian.kulmer@web.de>
2020-09-09 19:31:17 +0200

    drm: Expose panel_info sysfs node
    Change-Id: Ifafb2f5da69aa1e23cec3c9b7868afc32fee4970

--- drivers/gpu/drm/msm/dsi-staging/dsi_drm.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_drm.c
@@ -296,6 +296,22 @@ int dsi_bridge_interface_enable(int timeout)
 }
 EXPORT_SYMBOL(dsi_bridge_interface_enable);
 
+static int dsi_bridge_get_panel_info(struct drm_bridge *bridge, char *buf)
+{
+	int rc = 0;
+	struct dsi_bridge *c_bridge = to_dsi_bridge(bridge);
+
+	if (!c_bridge) {
+		pr_err("Invalid params\n");
+		return rc;
+	}
+
+	if (c_bridge->display->name)
+		return snprintf(buf, PAGE_SIZE, c_bridge->display->name);
+
+	return rc;
+}
+
 static void dsi_bridge_enable(struct drm_bridge *bridge)
 {
 	int rc = 0;
@@ -638,6 +654,7 @@ static const struct drm_bridge_funcs dsi_bridge_ops = {
 	.disable      = dsi_bridge_disable,
 	.post_disable = dsi_bridge_post_disable,
 	.mode_set     = dsi_bridge_mode_set,
+	.disp_get_panel_info = dsi_bridge_get_panel_info,
 };
 
 int dsi_conn_set_info_blob(struct drm_connector *connector,
