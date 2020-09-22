3f3825e041e9280788f535b0fec776e8fc6a1cde
Ziqi Chen <ziqichen@codeaurora.org>
2020-09-22 16:52:40 +0800

    scsi: ufs: Fix ufshcd_hold dead loop issue if error recovery is handing
    Commit 53c12d0ef6fc ("scsi: ufs: fix error recovery after the hibern8
    exit failure") would leave hba->clk_gating.active_reqs++ and skip
    subsequent actions in ufshcd_hold() if error handling is in progress.
    It may cause next ufschcd_release() queue a new clk gate work into
    hytimer even though the previous clk gate work has not yet got finish.
    Under this corner case, ufshcd_gate_work() may change uic_link_state
    to UIC_LINK_HIBERN8_STATE at the heels of setting clk state to CLK_ON
    by ufshcd_hold() and then run into ufshcd_hold dead loop. To fix this
    issue, we need to ensure there is no any pending and running clk gate
    work before changing clk state to CLK_ON in ufshcd_hold().
    
    Change-Id: I25fe35f2cad18f8a77fccf40755d856ee670594d
    Signed-off-by: Ziqi Chen <ziqichen@codeaurora.org>

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -2220,7 +2220,9 @@ start:
 		 * If the timer was active but the callback was not running
 		 * we have nothing to do, just change state and return.
 		 */
-		if (hrtimer_try_to_cancel(&hba->clk_gating.gate_hrtimer) == 1) {
+		if ((hrtimer_try_to_cancel(&hba->clk_gating.gate_hrtimer) == 1)
+			&& !(work_pending(&hba->clk_gating.gate_work))
+			&& !hba->clk_gating.gate_wk_in_process) {
 			hba->clk_gating.state = CLKS_ON;
 			trace_ufshcd_clk_gating(dev_name(hba->dev),
 						hba->clk_gating.state);
@@ -2272,7 +2274,9 @@ static void ufshcd_gate_work(struct work_struct *work)
 						clk_gating.gate_work);
 	unsigned long flags;
 
+	hba->clk_gating.gate_wk_in_process = true;
 	spin_lock_irqsave(hba->host->host_lock, flags);
+
 	if (hba->clk_gating.state == CLKS_OFF)
 		goto rel_lock;
 	/*
@@ -2351,6 +2355,7 @@ static void ufshcd_gate_work(struct work_struct *work)
 rel_lock:
 	spin_unlock_irqrestore(hba->host->host_lock, flags);
 out:
+	hba->clk_gating.gate_wk_in_process = false;
 	return;
 }
 
