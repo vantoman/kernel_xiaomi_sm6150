b489b265b0713a74f82619f5f1b130b5d5ef93df
Peter Zijlstra <peterz@infradead.org>
2020-10-01 15:54:14 +0200

    sched,rt: Use cpumask_any*_distribute()
    Replace a bunch of cpumask_any*() instances with
    cpumask_any*_distribute(), by injecting this little bit of random in
    cpu selection, we reduce the chance two competing balance operations
    working off the same lowest_mask pick the same CPU.
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Valentin Schneider <valentin.schneider@arm.com>
    Reviewed-by: Daniel Bristot de Oliveira <bristot@redhat.com>
    Link: https://lkml.kernel.org/r/20201023102347.190759694@infradead.org

--- kernel/sched/rt.c
+++ kernel/sched/rt.c
@@ -1930,8 +1930,8 @@ static int find_lowest_rq(struct task_struct *task)
 				return this_cpu;
 			}
 
-			best_cpu = cpumask_first_and(lowest_mask,
-						     sched_domain_span(sd));
+			best_cpu = cpumask_any_and_distribute(lowest_mask,
+							      sched_domain_span(sd));
 			if (best_cpu < nr_cpu_ids) {
 				rcu_read_unlock();
 				return best_cpu;
@@ -1948,7 +1948,7 @@ static int find_lowest_rq(struct task_struct *task)
 	if (this_cpu != -1)
 		return this_cpu;
 
-	cpu = cpumask_any(lowest_mask);
+	cpu = cpumask_any_distribute(lowest_mask);
 	if (cpu < nr_cpu_ids)
 		return cpu;
 	return -1;
