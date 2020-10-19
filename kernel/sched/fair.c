15bca9fe410d1c3fe861d92a84f718d2dff933c1
Rick Yiu <rickyiu@google.com>
2020-10-19 12:01:52 +0200

    sched/fair: schedule lower priority tasks from little cores
    With scheduler placement hint, there still could be several boosted
    tasks contending for big cores. On chipset with fewer big cores, it
    might cause problems like jank. To improve it, schedule tasks of prio
    >= DEFAULT_PRIO from little cores if they could fit, even for tasks
    that prefer high capacity cpus, since such prio means they are less
    important.
    
    Bug: 158936596
    Test: tasks scheduled as expected
    Signed-off-by: Rick Yiu <rickyiu@google.com>
    Change-Id: Ic0cc06461818944e3e97ec0493c0d9c9f1a5e217
    [backported to 4.14]
    Signed-off-by: Volodymyr Zhdanov <wight554@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7368,12 +7368,14 @@ static inline bool task_fits_capacity(struct task_struct *p,
 
 	if (capacity_orig_of(task_cpu(p)) > capacity_orig_of(cpu))
 		margin = schedtune_task_boost(p) > 0 &&
-			  !schedtune_prefer_high_cap(p) ?
+			  !schedtune_prefer_high_cap(p) &&
+			   p->prio <= DEFAULT_PRIO ?
 			sched_capacity_margin_down_boosted[task_cpu(p)] :
 			sched_capacity_margin_down[task_cpu(p)];
 	else
 		margin = schedtune_task_boost(p) > 0 &&
-			  !schedtune_prefer_high_cap(p) ?
+			  !schedtune_prefer_high_cap(p) &&
+			   p->prio <= DEFAULT_PRIO ?
 			sched_capacity_margin_up_boosted[task_cpu(p)] :
 			sched_capacity_margin_up[task_cpu(p)];
 
@@ -7440,7 +7442,7 @@ static int start_cpu(struct task_struct *p, bool boosted,
 	struct root_domain *rd = cpu_rq(smp_processor_id())->rd;
 	int start_cpu = -1;
 
-	if (boosted) {
+ 	if (boosted && p->prio <= DEFAULT_PRIO) {
 		if (rd->mid_cap_orig_cpu != -1 &&
 		    task_fits_max(p, rd->mid_cap_orig_cpu))
 			return rd->mid_cap_orig_cpu;
@@ -7870,7 +7872,7 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 		if ((prefer_idle && best_idle_cpu != -1) ||
 		    (prefer_high_cap &&
 		     (best_idle_cpu != -1 || target_cpu != -1))) {
-			if (prefer_high_cap) {
+ 			if (prefer_high_cap && p->prio <= DEFAULT_PRIO) {
 				/*
 				 * For prefer_high_cap task, stop searching when an idle
 				 * cpu is found in mid cluster.
