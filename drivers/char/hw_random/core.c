0672a54f4b4542008eaad0e9a146a380d9ee90ae
Stephen Boyd <swboyd@chromium.org>
2021-01-09 09:23:40 +0200

    hwrng: core - Freeze khwrng thread during suspend
    The hwrng_fill() function can run while devices are suspending and
    resuming. If the hwrng is behind a bus such as i2c or SPI and that bus
    is suspended, the hwrng may hang the bus while attempting to add some
    randomness. It's been observed on ChromeOS devices with suspend-to-idle
    (s2idle) and an i2c based hwrng that this kthread may run and ask the
    hwrng device for randomness before the i2c bus has been resumed.
    
    Let's make this kthread freezable so that we don't try to touch the
    hwrng during suspend/resume. This ensures that we can't cause the hwrng
    backing driver to get into a bad state because the device is guaranteed
    to be resumed before the hwrng kthread is thawed.
    
    Cc: Andrey Pronin <apronin@chromium.org>
    Cc: Duncan Laurie <dlaurie@chromium.org>
    Cc: Jason Gunthorpe <jgg@ziepe.ca>
    Cc: Arnd Bergmann <arnd@arndb.de>
    Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Cc: Guenter Roeck <groeck@chromium.org>
    Cc: Alexander Steffen <Alexander.Steffen@infineon.com>
    Signed-off-by: Stephen Boyd <swboyd@chromium.org>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- drivers/char/hw_random/core.c
+++ drivers/char/hw_random/core.c
@@ -12,6 +12,7 @@
 
 #include <linux/delay.h>
 #include <linux/device.h>
+#include <linux/freezer.h>
 #include <linux/err.h>
 #include <linux/fs.h>
 #include <linux/hw_random.h>
@@ -395,7 +396,9 @@ static int hwrng_fillfn(void *unused)
 {
 	long rc;
 
-	while (!kthread_should_stop()) {
+	set_freezable();
+
+	while (!kthread_freezable_should_stop(NULL)) {
 		struct hwrng *rng;
 
 		rng = get_current_rng();
