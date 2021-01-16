1760a39f48d23ab3776e136403920e6eb5bc5edb
vantoman <mustafa.vantom@gmail.com>
2021-01-16 09:09:40 +0200

    thermal: fix snprintf warning Fixes the following clang warning:
    drivers/thermal/thermal_core.c:1773:8: warning: 'snprintf' size argument is too large; destination buffer has size 128, but size argument is 4096 [-Wfortify-source]
           snprintf(board_sensor_temp, PAGE_SIZE, buf);
           ^
    1 warning generated.

--- drivers/thermal/thermal_core.c
+++ drivers/thermal/thermal_core.c
@@ -1770,7 +1770,7 @@ static ssize_t
 thermal_board_sensor_temp_store(struct device *dev,
 				struct device_attribute *attr, const char *buf, size_t len)
 {
-       snprintf(board_sensor_temp, PAGE_SIZE, buf);
+       snprintf(board_sensor_temp, sizeof(board_sensor_temp), buf);
 
        return len;
 }
