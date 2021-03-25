47e8886baccce99974968dbd3f749226d5ad24a9
Arian <arian.kulmer@web.de>
2021-03-25 15:56:29 +0100

    drm: msm: Implement demura dc dimming
    Change-Id: I3a91beeb766ed770d9f53238380433f20dcff7f4

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.h
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.h
@@ -37,6 +37,10 @@
 
 #define DSI_MODE_MAX 5
 
+#define DEMURA_LEVEL_02 256
+#define DEMURA_LEVEL_08 11
+#define DEMURA_LEVEL_0D 1
+
 enum dsi_panel_rotation {
 	DSI_PANEL_ROTATE_NONE = 0,
 	DSI_PANEL_ROTATE_HV_FLIP,
@@ -258,6 +262,15 @@ struct dsi_panel {
 
 	struct brightness_alpha_pair *fod_dim_lut;
 	u32 fod_dim_lut_count;
+
+	u32 last_bl_lvl;
+	u32 backlight_demura_level; /* For the f4_41 panel */
+	/* DC bkl */
+	bool dc_enable;
+	u32 dc_demura_threshold;
+	u32 dc_threshold;
+	u32 dc_type;
+	bool resend_dc;
 };
 
 static inline bool dsi_panel_ulps_feature_enabled(struct dsi_panel *panel)
@@ -387,5 +400,7 @@ int dsi_panel_set_fod_hbm(struct dsi_panel *panel, bool status);
 u32 dsi_panel_get_fod_dim_alpha(struct dsi_panel *panel);
 
 int dsi_panel_apply_hbm_mode(struct dsi_panel *panel);
+
+int dsi_panel_apply_dc_mode(struct dsi_panel *panel);
 
 #endif /* _DSI_PANEL_H_ */
