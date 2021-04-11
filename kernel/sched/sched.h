c2b69e78568a13fce21d0ec3803c36343348c7db
Diep Quynh <remilia.1505@gmail.com>
2021-04-12 04:07:50 +0700

    sched: fair: Account PELT estimated utilization on WALT disabled cpu_util_cum.
    This improves scheduler decision accuracy, on load balancing and EAS placement
    specifically
    
    Signed-off-by: Diep Quynh <remilia.1505@gmail.com>
    Signed-off-by: Vishalcj17 <vishalcj@aospa.co>
    Change-Id: I2f4c21c75b83db366646dd4ab302c35cf4e5342e

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -2049,7 +2049,7 @@ static inline unsigned long __cpu_util(int cpu)
 	unsigned int util;
 
 #ifdef CONFIG_SCHED_WALT
-	if (likely(!walt_disabled && sysctl_sched_use_walt_cpu_util)) {
+	if (!walt_disabled && sysctl_sched_use_walt_cpu_util) {
 		u64 walt_cpu_util =
 			cpu_rq(cpu)->walt_stats.cumulative_runnable_avg_scaled;
 
@@ -2076,13 +2076,15 @@ struct sched_walt_cpu_load {
 
 static inline unsigned long cpu_util_cum(int cpu, int delta)
 {
-	u64 util = cpu_rq(cpu)->cfs.avg.util_avg;
 	unsigned long capacity = capacity_orig_of(cpu);
+	u64 util;
 
 #ifdef CONFIG_SCHED_WALT
-	if (!walt_disabled && sysctl_sched_use_walt_cpu_util)
-		util = cpu_rq(cpu)->cum_window_demand_scaled;
+	util = cpu_util(cpu);
+#else
+	util = __cpu_util(cpu);
 #endif
+
 	delta += util;
 	if (delta < 0)
 		return 0;
