6e69f47fbf35718c4b610b958686c1d68f643844
Arian <arian.kulmer@web.de>
2021-04-26 14:22:58 +0200

    drm: msm: dsi-staging: Add panel flags for f4
    Change-Id: I961a47d6a4f39b6225e0d41e2888382e1ada1730

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.h
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.h
@@ -124,6 +124,8 @@ struct dsi_backlight_config {
 
 	bool dcs_type_ss_ea;
 	bool dcs_type_ss_eb;
+	bool xiaomi_f4_36_flag;
+	bool xiaomi_f4_41_flag;
 
 	/* PWM params */
 	struct pwm_device *pwm_bl;
