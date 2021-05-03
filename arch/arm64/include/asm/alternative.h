21ce9540b0d7cff11c9d7648095becb9568f81e0
vantoman <mustafa.vantom@gmail.com>
2021-05-03 22:45:16 +0200

    Revert "arm64: alternatives: Move length validation in alternative_{insn, endif}"
    This reverts commit b8ae95b946f3418a4f59cfb8229359d704f41707.

--- arch/arm64/include/asm/alternative.h
+++ arch/arm64/include/asm/alternative.h
@@ -117,7 +117,6 @@ void apply_alternatives(void *start, size_t length);
 664:	.previous
 	.org	. - (664b-663b) + (662b-661b)
 	.org	. - (662b-661b) + (664b-663b)
-	.popsection
 	.endif
 .endm
 
@@ -187,11 +186,11 @@ void apply_alternatives(void *start, size_t length);
  */
 .macro alternative_endif
 664:
-	.org	. - (664b-663b) + (662b-661b)
-	.org	. - (662b-661b) + (664b-663b)
 	.if .Lasm_alt_mode==0
 	.previous
 	.endif
+	.org	. - (664b-663b) + (662b-661b)
+	.org	. - (662b-661b) + (664b-663b)
 .endm
 
 /*
