dfd276e6ebbc1dfd08c53270b08bdecfafa3394c
Arian <arian.kulmer@web.de>
2021-07-03 17:44:13 +0200

    drm: msm: dsi-staging: Implement zero brightness doze mode
    Change-Id: I55b6248de4b060c89ee11e26c97bb38d0a80959b

--- drivers/gpu/drm/msm/dsi-staging/dsi_display.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_display.c
@@ -5151,7 +5151,7 @@ static ssize_t sysfs_doze_mode_write(struct device *dev,
 		return rc;
 	}
 
-	if (mode < DSI_DOZE_LPM || mode > DSI_DOZE_HBM) {
+	if (mode < DSI_DOZE_LPM || mode > DSI_DOZE_DARK) {
 		pr_err("%s: invalid value for doze mode\n", __func__);
 		return -EINVAL;
 	}
