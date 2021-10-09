bb4bee6efa872e2a03b6def392561f623b667fae
Demon000 <demonsingur@gmail.com>
2021-10-10 01:38:18 +0200

    input: touchscreen: fts_512: remove extra input sync from irq handler

--- drivers/input/touchscreen/fts_521/fts.c
+++ drivers/input/touchscreen/fts_521/fts.c
@@ -4381,7 +4381,6 @@ static void fts_ts_sleep_work(struct work_struct *work)
 			}
 		}
 	}
-	input_sync(info->input_dev);
 	info->irq_status = false;
 #ifdef CONFIG_TOUCHSCREEN_XIAOMI_TOUCHFEATURE
 	wake_up(&info->wait_queue);
@@ -4468,7 +4467,6 @@ static irqreturn_t fts_event_handler(int irq, void *ts_info)
 			}
 		}
 	}
-	input_sync(info->input_dev);
 	info->irq_status = false;
 #ifdef CONFIG_TOUCHSCREEN_XIAOMI_TOUCHFEATURE
 	wake_up(&info->wait_queue);
