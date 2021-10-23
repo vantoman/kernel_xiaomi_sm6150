7edb47853f9fdb692cf7ad69bed91164cb6adb11
Sultan Alsawaf <sultan@kerneltoast.com>
2021-10-23 19:25:23 +0200

    drm/msm/sde: Fix unchecked must_check DRM helper return values
    xNombre: Adapt to latest sde code
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Andrzej Perczak <linux@andrzejperczak.com>
    Change-Id: I6ac2752ea8e1283ee87d8c4190cc0efb7d24906c

--- drivers/gpu/drm/msm/sde/sde_kms.c
+++ drivers/gpu/drm/msm/sde/sde_kms.c
@@ -2020,6 +2020,12 @@ static int sde_kms_set_crtc_for_conn(struct drm_device *dev,
 	}
 
 	crtc_state = drm_atomic_get_crtc_state(state, enc->crtc);
+	if (IS_ERR(crtc_state)) {
+		SDE_ERROR("error %ld getting crtc %d state\n",
+				PTR_ERR(crtc_state), DRMID(conn));
+		return -EINVAL;
+	}
+
 	conn_state = drm_atomic_get_connector_state(state, conn);
 	if (IS_ERR(conn_state)) {
 		SDE_ERROR("error %ld getting connector %d state\n",
@@ -2029,8 +2035,10 @@ static int sde_kms_set_crtc_for_conn(struct drm_device *dev,
 
 	crtc_state->active = true;
 	ret = drm_atomic_set_crtc_for_connector(conn_state, enc->crtc);
-	if (ret)
+	if (ret) {
 		SDE_ERROR("error %d setting the crtc\n", ret);
+		return ret;
+	}
 
 	_sde_crtc_clear_dim_layers_v1(crtc_state);
 
@@ -2110,6 +2118,8 @@ static int _sde_kms_remove_fbs(struct sde_kms *sde_kms, struct drm_file *file,
 					crtc->state->encoder_mask)
 				ret = sde_kms_set_crtc_for_conn(
 					dev, drm_enc, state);
+				if (ret)
+					goto end;
 		}
 	}
 
@@ -2789,7 +2799,9 @@ retry:
 	if (ret)
 		goto end;
 
-	drm_atomic_commit(state);
+	ret = drm_atomic_commit(state);
+	if (ret)
+		SDE_ERROR("error %d committing state for connector\n", ret);
 end:
 	if (state)
 		drm_atomic_state_put(state);
