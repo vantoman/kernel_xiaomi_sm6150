035fee37a43cb3a2172ac25d75b168a5cbfe88dc
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-11-30 23:36:46 +0530

    drm/msm: msm_gem: Remove unused `fail` label
    Since 1e28fc765742dc78068c5d1509eee135c96db94c
    
    ../drivers/gpu/drm/msm/msm_gem.c:1303:1: warning: unused label 'fail' [-Wunused-label]
    fail:
    ^~~~~

--- drivers/gpu/drm/msm/msm_gem.c
+++ drivers/gpu/drm/msm/msm_gem.c
@@ -1299,10 +1299,6 @@ struct drm_gem_object *msm_gem_import(struct drm_device *dev,
 
 	mutex_unlock(&msm_obj->lock);
 	return obj;
-
-fail:
-	drm_gem_object_unreference_unlocked(obj);
-	return ERR_PTR(ret);
 }
 
 static void *_msm_gem_kernel_new(struct drm_device *dev, uint32_t size,
