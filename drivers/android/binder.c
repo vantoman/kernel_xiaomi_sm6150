0f27eaec65e85dfab4126ee844e813774e696044
Carlos Jimenez (JavaShin-X) <javashin1986@gmail.com>
2021-12-04 13:49:36 -0400

    ANDROID: binder: Remove Leftover -ERESTARTSYS
    still causing spam lets kill it
    
    Signed-off-by: Carlos Jimenez (JavaShin-X) <javashin1986@gmail.com>

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -982,8 +982,7 @@ static long task_close_fd(struct binder_proc *proc, unsigned int fd)
 	}
 	retval = __close_fd(proc->files, fd);
 	/* can't restart close syscall because file table entry was cleared */
-	if (unlikely(retval == -ERESTARTSYS ||
-		     retval == -ERESTARTNOINTR ||
+	if (unlikely(retval == -ERESTARTNOINTR ||
 		     retval == -ERESTARTNOHAND ||
 		     retval == -ERESTART_RESTARTBLOCK))
 		retval = -EINTR;
