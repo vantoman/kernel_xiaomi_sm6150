7560e74652c35f7162adef471e3a59950f87c59b
Fiqri Ardyansyah <fiqri15072019@gmail.com>
2021-12-21 00:07:34 +0700

    sched/fair: Move lockdep_*() under CONFIG_SCHED_WALT in detach_task()
    No need on a scheduler other than WALT.
    
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -9397,6 +9397,7 @@ static void detach_task(struct task_struct *p, struct lb_env *env)
 	p->on_rq = TASK_ON_RQ_MIGRATING;
 	deactivate_task(env->src_rq, p, DEQUEUE_NOCLOCK);
 #ifdef CONFIG_SCHED_WALT
+	lockdep_off();
 	double_lock_balance(env->src_rq, env->dst_rq);
 	if (!(env->src_rq->clock_update_flags & RQCF_UPDATED))
 		update_rq_clock(env->src_rq);
