fe5c28a6824150612764b33da70c6f5625df124e
Nauval Rizky <enuma.alrizky@gmail.com>
2022-01-30 07:18:44 +0700

    power: supply: ti: Fix usb state checking for ln8000
    Clang warns:
    kernel/xiaomi/vayu/drivers/power/supply/ti/cp_qc30.c:1022:9: warning: incompatible pointer to integer conversion returning 'struct power_supply *' from a function with result type 'int' [-Wint-conversion]
            return power_supply_get_by_name("ln8000");
                   ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    kernel/xiaomi/vayu/drivers/power/supply/ti/cp_qc30.c:1039:7: warning: address of function 'cp_psy_is_lionsemi' will always evaluate to 'true' [-Wpointer-bool-conversion]
                    if (cp_psy_is_lionsemi)
                    ~~  ^~~~~~~~~~~~~~~~~~

--- drivers/power/supply/ti/cp_qc30.c
+++ drivers/power/supply/ti/cp_qc30.c
@@ -1203,13 +1203,9 @@ void cp_statemachine(unsigned int port)
 	}
 }
 
-static int cp_psy_is_lionsemi(void)
-{
-	return power_supply_get_by_name("ln8000");
-}
-
 static void cp_workfunc(struct work_struct *work)
 {
+	struct power_supply *psy;
 	cp_get_usb_type();
 
 	cp_update_sw_status();
@@ -1222,8 +1218,10 @@ static void cp_workfunc(struct work_struct *work)
 	if (pm_state.usb_present == 0) {
 		cp_set_qc_bus_protections(HVDCP3_NONE);
 		cp_set_fake_hvdcp3(false);
-		if (cp_psy_is_lionsemi)
+		psy = power_supply_get_by_name("ln8000");
+		if (psy)
 			pm_state.state = CP_STATE_DISCONNECT;
+
 		return;
 
 	}
