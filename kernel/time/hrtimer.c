59004a28879a35f2dc365f41b9779056bcd547eb
Sultan Alsawaf <sultan@kerneltoast.com>
2022-02-15 01:18:15 +0530

    RELAND: Revert "kernel: time: Add delay after cpu_relax() in tight loops"
    We have queued spin locks, so spin lock starvation isn't a problem.
    
    Signed-off-by: Jebaitedneko <Jebaitedneko@gmail.com>

--- kernel/time/hrtimer.c
+++ kernel/time/hrtimer.c
@@ -52,7 +52,6 @@
 #include <linux/timer.h>
 #include <linux/freezer.h>
 #include <linux/compat.h>
-#include <linux/delay.h>
 
 #include <linux/uaccess.h>
 
@@ -185,7 +184,6 @@ struct hrtimer_clock_base *lock_hrtimer_base(const struct hrtimer *timer,
 			raw_spin_unlock_irqrestore(&base->cpu_base->lock, *flags);
 		}
 		cpu_relax();
-		ndelay(TIMER_LOCK_TIGHT_LOOP_DELAY_NS);
 	}
 }
 
