176a6cb0e1f88cdcf71f7caac7563101303f3399
Forenche <prahul2003@gmail.com>
2022-03-16 10:44:13 +0530

    f2fs: Fix kernel panic after `f2fs-stable` merge
    - [1] added inline encryption support in F2FS yet one important snippet was dropped in [2].
    - This caused a kernel panic regarding fscrypt at init.
    - Reverting changes done to the snippet fixed boot.
    
    [1]: e64327f5719b4 {"BACKPORT: FROMLIST: f2fs: add inline encryption support"}
    [2]: f23c8d887eab {"F2FS: Merge branch `upstream-f2fs-stable-linux-4.14.y` of `msm-4.14`"}
    
    [cyberknight777: reword commit message and description]
    Signed-off-by: Forenche <prahul2003@gmail.com>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- fs/f2fs/data.c
+++ fs/f2fs/data.c
@@ -1037,8 +1037,8 @@ static struct bio *f2fs_grab_read_bio(struct inode *inode, block_t blkaddr,
 	bio->bi_end_io = f2fs_read_end_io;
 	bio_set_op_attrs(bio, REQ_OP_READ, op_flag);
 
-	if (f2fs_encrypted_file(inode))
-		post_read_steps |= STEP_DECRYPT;
+	if (fscrypt_inode_uses_fs_layer_crypto(inode))
+		post_read_steps |= 1 << STEP_DECRYPT;
 
 	if (f2fs_need_verity(inode, first_idx))
 		post_read_steps |= STEP_VERITY;
