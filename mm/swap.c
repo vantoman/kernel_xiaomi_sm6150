a6f44356677b9fa49159b12ada92182f6486365b
vantoman <Mustafa.vantom@gmail.com>
2022-07-21 12:21:08 +0200

    Revert "BACKPORT: mm: add __clear_page_lru_flags() to replace page_off_lru()"
    This reverts commit 889060e7a439324b91d292b3f6cfe9da3dd820e9.

--- mm/swap.c
+++ mm/swap.c
@@ -66,8 +66,9 @@ static void __page_cache_release(struct page *page)
 		spin_lock_irqsave(zone_lru_lock(zone), flags);
 		lruvec = mem_cgroup_page_lruvec(page, zone->zone_pgdat);
 		VM_BUG_ON_PAGE(!PageLRU(page), page);
+		__ClearPageLRU(page);
 		del_page_from_lru_list(page, lruvec);
-		__clear_page_lru_flags(page);
+		page_off_lru(page);
 		spin_unlock_irqrestore(zone_lru_lock(zone), flags);
 	}
 	__ClearPageWaiters(page);
@@ -838,8 +839,9 @@ void release_pages(struct page **pages, int nr, bool cold)
 
 			lruvec = mem_cgroup_page_lruvec(page, locked_pgdat);
 			VM_BUG_ON_PAGE(!PageLRU(page), page);
+			__ClearPageLRU(page);
 			del_page_from_lru_list(page, lruvec);
-			__clear_page_lru_flags(page);
+			page_off_lru(page);
 		}
 
 		__ClearPageWaiters(page);
