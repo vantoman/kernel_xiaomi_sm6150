71d1116057d57e256bbc9948c68eecf1cbaa07bd
vantoman <Mustafa.vantom@gmail.com>
2022-07-21 12:21:06 +0200

    Revert "BACKPORT: FROMLIST: mm: multigenerational lru: groundwork"
    This reverts commit d4d11ae965655762b756a31fd7e82a9ad59dd7c1.

--- fs/fuse/dev.c
+++ fs/fuse/dev.c
@@ -838,8 +838,7 @@ static int fuse_check_page(struct page *page)
 	       1 << PG_uptodate |
 	       1 << PG_lru |
 	       1 << PG_active |
-	       1 << PG_reclaim |
-	       LRU_GEN_MASK | LRU_USAGE_MASK))) {
+	       1 << PG_reclaim))) {
 		printk(KERN_WARNING "fuse: trying to steal weird page\n");
 		printk(KERN_WARNING "  page=%p index=%li flags=%08lx, count=%i, mapcount=%i, mapping=%p\n", page, page->index, page->flags, page_count(page), page_mapcount(page), page->mapping);
 		return 1;
