904e6b06baa271d23154cba3401c1f1f684fca08
vantoman <Mustafa.vantom@gmail.com>
2022-07-21 11:02:26 +0200

    Revert "BACKPORT: mm/swapcache: support to handle the shadow entries"
    This reverts commit f830313e4804b9bf18cf72330e3462aeb709f063.

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -728,7 +728,7 @@ static int __remove_mapping(struct address_space *mapping, struct page *page,
 	if (PageSwapCache(page)) {
 		swp_entry_t swap = { .val = page_private(page) };
 		mem_cgroup_swapout(page, swap);
-		__delete_from_swap_cache(page, NULL);
+		__delete_from_swap_cache(page);
 		spin_unlock_irqrestore(&mapping->tree_lock, flags);
 		put_swap_page(page, swap);
 	} else {
