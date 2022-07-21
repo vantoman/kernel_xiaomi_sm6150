d56ed23e5ec0ba96921136e2206a73864f49bf13
vantoman <Mustafa.vantom@gmail.com>
2022-07-21 12:20:16 +0200

    Revert "mm/vmscan: Fix build after MGLRU patchset [1/3]"
    This reverts commit 713d0666bbe14b3f68a2957079ff713786e5c2ac.

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -42,7 +42,6 @@
 #include <linux/delay.h>
 #include <linux/kthread.h>
 #include <linux/freezer.h>
-#include <linux/overflow.h>
 #include <linux/memcontrol.h>
 #include <linux/delayacct.h>
 #include <linux/sysctl.h>
@@ -4440,6 +4439,7 @@ void lru_gen_set_state(bool enable, bool main, bool swap)
 
 	mem_hotplug_begin();
 	mutex_lock(&lru_gen_state_mutex);
+	cgroup_lock();
 
 	main = main && enable != lru_gen_enabled();
 	swap = swap && !(enable ? lru_gen_nr_swapfiles++ : --lru_gen_nr_swapfiles);
@@ -4484,6 +4484,7 @@ void lru_gen_set_state(bool enable, bool main, bool swap)
 		cond_resched();
 	} while ((memcg = mem_cgroup_iter(NULL, memcg, NULL)));
 unlock:
+	cgroup_unlock();
 	mutex_unlock(&lru_gen_state_mutex);
 	mem_hotplug_done();
 }
@@ -4499,6 +4500,7 @@ static int __meminit __maybe_unused lru_gen_online_mem(struct notifier_block *se
 		return NOTIFY_DONE;
 
 	mutex_lock(&lru_gen_state_mutex);
+	cgroup_lock();
 
 	memcg = mem_cgroup_iter(NULL, NULL, NULL);
 	do {
@@ -4512,6 +4514,7 @@ static int __meminit __maybe_unused lru_gen_online_mem(struct notifier_block *se
 		WRITE_ONCE(lrugen->enabled[1], lru_gen_enabled());
 	} while ((memcg = mem_cgroup_iter(NULL, memcg, NULL)));
 
+	cgroup_unlock();
 	mutex_unlock(&lru_gen_state_mutex);
 
 	return NOTIFY_DONE;
@@ -4699,7 +4702,7 @@ static void lru_gen_seq_show_full(struct seq_file *m, struct lruvec *lruvec,
 static int lru_gen_seq_show(struct seq_file *m, void *v)
 {
 	unsigned long seq;
-	bool full = false;
+	bool full = !debugfs_real_fops(m->file)->write;
 	struct lruvec *lruvec = v;
 	struct lrugen *lrugen = &lruvec->evictable;
 	int nid = lruvec_pgdat(lruvec)->node_id;
