a0a31ee5821bb3912b12b7edb8916cbb9e47e7ed
Cyber Knight <cyberknight755@gmail.com>
2022-09-21 16:13:52 +0000

    Revert "simple_lmk: increase vmpressure threshold"
    This reverts commit 177a7c0817a5c84f49590adf59c020fcd04b49a4.
    
    Change-Id: I5b9d5c9c53a8a69dbee94b0bc3d59bcd8ed70b58

--- drivers/android/simple_lmk.c
+++ drivers/android/simple_lmk.c
@@ -315,7 +315,7 @@ void simple_lmk_trigger(void)
 static int simple_lmk_vmpressure_cb(struct notifier_block *nb,
 				    unsigned long pressure, void *data)
 {
-	if (pressure >= 90)
+	if (pressure >= 80)
 		simple_lmk_trigger();
 
 	return NOTIFY_OK;
